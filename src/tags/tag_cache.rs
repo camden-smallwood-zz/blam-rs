4ac56cfa7c7c160080fac5337d27acdd61262e9f
Camden Smallwood <camden.smallwood@gmail.com>
2018-12-05 20:14:55 -0500

    More safety refactoring to tag cache code

--- src/tags/tag_cache.rs
+++ src/tags/tag_cache.rs
@@ -41,48 +41,57 @@ pub fn get_mut_ptr<T>(offset: isize) -> io::Result<*mut T> {
     }
 }
 
-pub fn get_header<'a>() -> io::Result<Option<&'a TagCacheHeader>> {
-    Ok(unsafe { self::get_ptr::<TagCacheHeader>(0)?.as_ref() })
+pub fn get_header<'a>() -> io::Result<&'a TagCacheHeader> {
+    Ok(unsafe { self::get_ptr::<TagCacheHeader>(0)?.as_ref().unwrap() })
 }
 
-pub fn get_offset(tag_index: isize) -> io::Result<isize> {
-    if let Some(ref header) = self::get_header()? {
+pub fn get_offset(tag_index: isize) -> io::Result<Option<isize>> {
+    let header = self::get_header()?;
+    if tag_index < 0 || tag_index >= header.instance_count as isize {
+        Err(io::Error::new(io::ErrorKind::InvalidInput, format!("Invalid tag index: 0x{:X}", tag_index).to_string()))
+    } else {
         let tag_offsets = self::get_ptr::<i32>(header.index_offset as isize)?;
         if tag_offsets.is_null() {
             Err(io::Error::new(io::ErrorKind::AddrNotAvailable, "Tag index address is null"))
         } else {
-            let result = unsafe { *(tag_offsets.offset(tag_index)) as isize };
-            if result <= 0 {
-                Err(io::Error::new(io::ErrorKind::AddrNotAvailable, "Tag has null offset"))
+            let tag_offset = unsafe { *(tag_offsets.offset(tag_index)) as isize };
+            if tag_offset <= 0 {
+                Ok(None)
             } else {
-                Ok(result)
+                Ok(Some(tag_offset))
             }
         }
-    } else {
-        Err(io::Error::new(io::ErrorKind::NotFound, "Tag cache has no header"))
     }
 }
 
 pub fn get_instance<'a>(tag_index: isize) -> io::Result<Option<&'a TagInstance>> {
-    Ok(unsafe { self::get_ptr::<TagInstance>(self::get_offset(tag_index)?)?.as_ref() })
-}
-
-pub fn get_instance_mut<'a>(tag_index: isize) -> io::Result<Option<&'a mut TagInstance>> {
-    Ok(unsafe { self::get_mut_ptr::<TagInstance>(self::get_offset(tag_index)?)?.as_mut() })
-}
-
-pub fn get_definition<'a, T: TagGroupDefinition>(tag_index: isize) -> io::Result<Option<&'a T>> {
-    if let Some(ref instance) = self::get_instance(tag_index)? {
-        Ok(unsafe { self::get_ptr::<T>(self::get_offset(tag_index)? + instance.definition_offset as isize)?.as_ref() })
+    if let Some(offset) = self::get_offset(tag_index)? {
+        Ok(unsafe { self::get_ptr::<TagInstance>(offset)?.as_ref() })
     } else {
-        Err(io::Error::new(io::ErrorKind::NotFound, format!("Tag index 0x{:X} is null", tag_index).to_string()))
+        Ok(None)
     }
 }
 
-pub fn get_definition_mut<'a, T: TagGroupDefinition>(tag_index: isize) -> io::Result<Option<&'a mut T>> {
-    if let Some(ref instance) = self::get_instance(tag_index)? {
-        Ok(unsafe { self::get_mut_ptr::<T>(self::get_offset(tag_index)? + instance.definition_offset as isize)?.as_mut() })
+pub fn get_instance_mut<'a>(tag_index: isize) -> io::Result<Option<&'a mut TagInstance>> {
+    if let Some(offset) = self::get_offset(tag_index)? {
+        Ok(unsafe { self::get_mut_ptr::<TagInstance>(offset)?.as_mut() })
     } else {
-        Err(io::Error::new(io::ErrorKind::NotFound, format!("Tag index 0x{:X} is null", tag_index).to_string()))
+        Ok(None)
+    }
+}
+
+pub fn get_definition<'a, T: TagGroupDefinition>(tag_index: isize) -> io::Result<&'a T> {
+    if let Some(ref instance) = self::get_instance(tag_index)? {
+        Ok(unsafe { self::get_ptr::<T>(self::get_offset(tag_index)?.unwrap() + instance.definition_offset as isize)?.as_ref().unwrap() })
+    } else {
+        Err(io::Error::new(io::ErrorKind::NotFound, format!("Tag instance 0x{:X} is null", tag_index).to_string()))
+    }
+}
+
+pub fn get_definition_mut<'a, T: TagGroupDefinition>(tag_index: isize) -> io::Result<&'a mut T> {
+    if let Some(ref instance) = self::get_instance(tag_index)? {
+        Ok(unsafe { self::get_mut_ptr::<T>(self::get_offset(tag_index)?.unwrap() + instance.definition_offset as isize)?.as_mut().unwrap() })
+    } else {
+        Err(io::Error::new(io::ErrorKind::NotFound, format!("Tag instance 0x{:X} is null", tag_index).to_string()))
     }
 }
